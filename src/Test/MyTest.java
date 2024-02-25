5b842c0ce074875ff94311dfab23a00108bc28cd
Anurag <srivastava.anurag1989@gmail.com>
2024-02-25 23:28:50 +0530

    cart page added

--- src/Test/MyTest.java
+++ src/Test/MyTest.java
@@ -4,6 +4,8 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("neww");	}
+System.out.println("neww");
+System.out.println("neww");
+System.out.println("neww");}
 
 }
